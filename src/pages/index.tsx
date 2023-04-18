06f0983ace2ca420f426bb1b9c671a3cab17dfca
AIllarionov <hellomyguest@yandex.ru>
2023-04-18 11:48:24 +0300

    feat(components): added lazy loading for pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,40 @@
+import { Suspense, lazy } from "react";
+import { Skeleton, Spin } from "antd";
 import { Route } from "atomic-router-react";
 import { routes } from "../shared/lib/atomic-router/routes";
-import { HomePage } from "./home/page";
-import { ResumePage } from "./resume/page";
-import { PortfolioPage } from "./portfolio/page";
-import { GrowthPage } from "./growth/page";
-import { ContactsPage } from "./contacts/page";
+
+const HomePage = lazy(() =>
+  import("./home/page").then((module) => ({ default: module.HomePage }))
+);
+const ResumePage = lazy(() =>
+  import("./resume/page").then((module) => ({ default: module.ResumePage }))
+);
+const PortfolioPage = lazy(() =>
+  import("./portfolio/page").then((module) => ({
+    default: module.PortfolioPage,
+  }))
+);
+const GrowthPage = lazy(() =>
+  import("./growth/page").then((module) => ({ default: module.GrowthPage }))
+);
+const ContactsPage = lazy(() =>
+  import("./contacts/page").then((module) => ({ default: module.ContactsPage }))
+);
 
 export const Pages = () => (
-  <>
+  <Suspense
+    fallback={
+      <Spin size="large">
+        <Skeleton paragraph={{ rows: 11 }} />
+      </Spin>
+    }
+  >
     <Route route={routes.home} view={HomePage} />
     <Route route={routes.resume} view={ResumePage} />
     <Route route={routes.portfolio} view={PortfolioPage} />
     <Route route={routes.growth} view={GrowthPage} />
     <Route route={routes.contacts} view={ContactsPage} />
-  </>
+  </Suspense>
 );
 
 export const routesMap = [
